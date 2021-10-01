800b5e968975873f21df4836a1c970bdc41ffaf8
Alice Campkin <alice.campkin@gmail.com>
2021-10-01 23:56:44 +0100

    Add Todo model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=255)
+    notes = models.TextField(blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=255, default='todo')
+
+    def __str__(self):
+        return self.title
--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Todo
 
 # Create your tests here.
+
+todo_title = "Book Dentist Appointment"
+
+class TestTodoModel(TestCase):
+
+    def setUp(self):
+        self.todo = Todo.objects.create(
+            title=todo_title
+        )
+    
+    def test_todo_created(self):
+        self.assertEqual(self.todo.title, todo_title)
+    
+    def test_date_automatically_assigned(self):
+        self.assertTrue(self.todo.created_on)
+
+    def test_status_assigned(self):
+        self.assertEqual(self.todo.status, "todo")
+    
+    def test_str(self):
+        self.assertEqual(str(self.todo), todo_title)
