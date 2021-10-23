10f5a75d7bb04ebff378c4811efed6493df3c40a
Alice Campkin <alice.campkin@zoodigital.com>
2021-10-23 15:41:41 +0100

    Use boolean to represent task status

--- todo/models.py
+++ todo/models.py
@@ -5,7 +5,7 @@ class Todo(models.Model):
     title = models.CharField(max_length=255)
     notes = models.TextField(blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=255, default='todo')
+    completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
