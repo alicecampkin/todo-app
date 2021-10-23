1bed447d0ee822ed5563862a05b1e63bdd6e541d
Alice Campkin <alice.campkin@zoodigital.com>
2021-10-23 16:41:38 +0100

    Tutorial Part 3 - add first URL, View and Template

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Todo
+
 # Create your views here.
+
+def list_todos(request):
+    """ Displays a list of posts """
+
+    todos = Todo.objects.filter(completed=False).order_by('-created_on')
+
+    context = {
+        'page_title': 'To Do List',
+        'todos': todos
+    }
+
+    return render(request, 'index.html', context)
